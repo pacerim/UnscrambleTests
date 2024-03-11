e61016e68773cb3e1f34557a40d448509e183c21
diazpabloo <pdiazdecerio3@gmail.com>
2024-03-11 12:29:54 +0100

    2

---
+++ app/src/test/java/com/example/unscramble/ui/test/GameViewModelTest.kt
@@ -0,0 +1,31 @@
+package com.example.unscramble.ui.test
+
+import com.example.unscramble.data.SCORE_INCREASE
+import com.example.unscramble.data.getUnscrambledWord
+import com.example.unscramble.ui.GameViewModel
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertFalse
+import org.junit.Test
+
+class GameViewModelTest {
+    private val viewModel = GameViewModel()
+
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset()  {
+        var currentGameUiState = viewModel.uiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+
+        viewModel.updateUserGuess(correctPlayerWord)
+        viewModel.checkUserGuess()
+
+        currentGameUiState = viewModel.uiState.value
+        // Assert that checkUserGuess() method updates isGuessedWordWrong is updated correctly.
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        // Assert that score is updated correctly.
+        assertEquals(20, currentGameUiState.score)
+    }
+
+    companion object {
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = SCORE_INCREASE
+    }
+}
